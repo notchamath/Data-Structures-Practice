7f3036899adf3ce35a79ca53e94ec7ccb55725ae
Chamath Jayarathne <notchamath@aol.com>
2020-10-19 21:38:45 -0400

    Merge Sorted Arrays program added

---
+++ A function that merges 2 Sorted Arrays/mergeSortedArrays.js
@@ -0,0 +1,48 @@
+//this program takes 2 sorted arrays of integers and merges them
+
+function merge(mergedArr, arr1, arr2) {
+
+    let i = 0,
+        j = 0;
+    let arr1Item = arr1[j],
+        arr2Item = arr2[i];
+
+    while (arr1Item || arr2Item) {
+        if (arr1Item > arr2Item || arr1Item === undefined) {
+
+            mergedArr.push(arr2Item);
+            i++;
+            arr2Item = arr2[i];
+
+        } else {
+
+            mergedArr.push(arr1Item);
+            j++;
+            arr1Item = arr1[j];
+
+        }
+    }
+}
+
+function mergeSortedArrays(arr1, arr2) {
+    //check if inputs are valid
+    if (!Array.isArray(arr1) || !Array.isArray(arr2)) {
+        return 'The inputs are not arrays';
+    }
+
+    if (arr1.length === 0) {
+        return arr2;
+    }
+
+    if (arr2.length === 0) {
+        return arr1;
+    }
+
+    const mergedArr = [];
+
+    merge(mergedArr, arr1, arr2);
+
+    return mergedArr;
+}
+
+mergeSortedArrays([0, 7, 10, 17, 31], [3, 4, 6, 30])
