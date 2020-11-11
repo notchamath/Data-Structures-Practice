76c3f85d2887b77425c5b212269a32ca7025492f
Chamath Jayarathne <notchamath@aol.com>
2020-11-11 11:50:48 -0500

    Bubble Sort Added

---
+++ Bubble Sort Implementation/bubbleSort.js
@@ -0,0 +1,23 @@
+//creating a function that sorts an array using Bubble Sort
+//Bubble sort has a time complexity of O(n^2) because of the nested loops and a space comeplexity of O(1)
+
+function bubbleSort(arr){
+    let length = arr.length;
+   
+    for(let i=0; i<length; i++){
+        for(let j=0; j<length; j++){
+   
+            if(arr[j] > arr[j+1]){
+                let temp = arr[j+1];
+                arr[j+1] = arr[j];
+                arr[j] = temp;
+            }
+   
+        }
+     }
+   
+     return arr;
+   }
+   
+   let arr = [5,2,88,2,45,4,11,0,3,2,36]
+   bubbleSort(arr);
