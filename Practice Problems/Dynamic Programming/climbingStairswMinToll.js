552c690957e9849d533f506e2da5eb051f45d35f
Chamath Jayarathne <chamathsocial@gmail.com>
2020-11-25 01:30:21 -0500

    Climbing Stairs with Minimum Tolls added

--- Practice Problems/Dynamic Programming/climbingStairswMinToll.js
+++ Practice Problems/Dynamic Programming/climbingStairswMinToll.js
@@ -2,23 +2,37 @@
 //What is the best(least costly) way to climb to the nth stair if each step has a cost and you can take k amount of steps at a time?
 
 
+//time complexity: O(nk)
+//Space complexity: O(k)
 function climbStairs(n, k, toll){
-    let cache = [n + 1];
+
+    //use cache to store tolls for each step(after calculation) and temp to store prev k values
+    let cache = [k];
+	let temp = [k];
+
+    //base cases
     cache[0] = 0;
     cache[1] = toll[1];
 
+    //for each step i calculate tolls by using equation which adds the k previous step's tolls to current steps toll
     for(let i=2; i<=n; i++){
-        cache[i] = toll[i] + Math.min(cache[i - 1], cache[i - 2]);
+        for(let j=1; j<=k; j++){
+            if(i-j >= 0){
+                temp[j-1] = cache[(i-j) % k];
+            }
+        }
+        cache[i % k] = toll[i] + Math.min(...temp);
     }
 
-    return cache[n + 1];
+    //return least expensive route
+    return cache[n % k];
 }
 
 // n = the target stair
 // k = maximum steps you can take at a time
 // toll = toll for stepping on each stair
 
-let n = 4;
+let n = 3;
 let k = 2;
-let toll = [];
+let toll = [0,3,2,4];
 climbStairs(n, k, toll);
