86dcf02aba22f9b08fdd06c0d1d111886b641a68
Chamath Jayarathne <chamathsocial@gmail.com>
2020-11-29 02:59:14 -0500

    Traversing Matrix Problem Added + directory Rearranged

---
+++ Practice Problems/Dynamic Programming/Traversing Matrix Problems/TraverseMatrix.js
@@ -0,0 +1,36 @@
+//Problem: How many ways are there to get to point (i,j) if you started at point (0,0)?
+
+//function takes in the size of the matrix and outputs the num of different ways to get to the final point of (m,n)
+function uniquePath(m, n){
+
+	//create multidimentional array of size (m x n)
+	let dp = [m];
+	for(let i = 0; i < m; i++){
+			dp[i] = [n];
+	}
+
+	//base case
+	dp[0][0] = 1;
+
+	//keep adding the num of ways to get to the point leading up to the point you want to get to 
+	for(let i = 0; i < m; i++){
+		for(let j = 0; j < n; j++){
+			if(i > 0 && j > 0){
+				dp[i][j] = dp[i-1][j] + dp[i][j-1];
+			} else if(i > 0){
+				dp[i][j] = dp[i-1][j];
+			} else if(j > 0){
+				dp[i][j] = dp[i][j-1];
+			}
+		}
+	}
+	return dp[m-1][n-1];
+}
+
+
+//m = height of matrix
+//n = width of matrix
+let m = 3;
+let n = 4;
+
+uniquePath(m, n);
